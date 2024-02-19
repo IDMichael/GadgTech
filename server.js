40cb60481bd26a72db428edf7d952e90b1be2b0a
Michael Ukana <alltymz@gmail.com>
2024-02-19 12:51:14 +0100

    New Changes

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function(request,response){
-    res.sendFile(path.join(__dirname,'public'/index.html));
-});
+// app.get('/', function(request,response) {
+//     res.sendFile(path.join(__dirname,'public'/index.html));
+// });
 
-app.get('*', function(request, response){"Server is running"});
-app.listen(3000, function(){"Server is running"});
-
-// app.get('/', (req,res) => {
+// app.get('/home', (req,res) => {
 //     res.sendFile(path.join(__dirname,'public'/home.html));
 // });
 
-// app.get('/', (req,res) => {
+// app.get('/products', (req,res) => {
 //     res.sendFile(path.join(__dirname,'public'/products.html));
 // });
 
-// app.get('/', (req,res) => {
+// app.get('/about', (req,res) => {
 //     res.sendFile(path.join(__dirname,'public'/about.html));
 // });
 
-// app.get('/', (req,res) => {
+// app.get('/contacts', (req,res) => {
 //     res.sendFile(path.join(__dirname,'public'/contacts.html));
 // });
 
-// app.get('/', (req,res) => {
+// app.get('/account', (req,res) => {
 //     res.sendFile(path.join(__dirname,'public'/account.html));
 // });
 
 // app.listen(port, () => {
-//     console.log('Server is running at http://localhost:${port}');
+//     console.log('My Simple Server is running at http://localhost:${port}');
 // });
+
+app.get('/', function(req, res) {
+    res.send('My First Simple Server!');
+});
+
+app.listen(port, () => {
+    console.log('My Simple Server is running at port 4000!');
+});
